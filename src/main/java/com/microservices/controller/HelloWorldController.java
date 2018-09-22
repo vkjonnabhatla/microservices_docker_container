4e426ebef568d172cf8f4209427fe43d252ad7ba
Venkatakrishna Jonnabatla <venkatakrishna_jonnabatla@gap.com>
2018-09-22 14:33:59 +0530

    Spring boot docker container example

---
+++ src/main/java/com/microservices/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.microservices.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/sayHello")
+    public ResponseEntity sayHello(@RequestParam String name){
+        return new ResponseEntity("Hi "+ name +", Welcome to the world of Microservices." , HttpStatus.OK);
+    }
+}
